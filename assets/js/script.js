37fd5d7b2daae298f1e4328689d785fe7698abd3
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-06-14 07:19:55 -0500

    Add question functionality to replace initial page content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 // function for countdown timer
 
 timerInitial = 60;
-let startTimer = function() {
+let startTimer = function () {
     document.getElementById("timer").innerHTML = timerInitial;
     timerInitial--;
     if (timerInitial < 0) {
         alert("The timer has expired!")
-    } 
+    }
     else {
         setTimeout(startTimer, 1000);
     }
@@ -15,30 +15,51 @@ let startTimer = function() {
 
 // functions for individual questions
 
-let startQuiz = function() {
-    alert("The quiz has begun!");
-};
-    
-    // write question to screen (addContent)
-    
-    // if else for answer selection
+let startQuiz = function () {
+    let pageContentEl = document.getElementById("page-content");
+    pageContentEl.remove();
 
-        // if true, advance to next question
-        // if false, subtract 10 seconds from timer, advance to next question (removeContent, addContent)
+    let questionOne = function () {
+        // create new div element
+        let pageContentEl = document.createElement("section");
+
+        // adding content to new section element
+        let questionOneContent = document.createTextNode("This is the content of the first question.");
+
+        // adding id to new section element
+        pageContentEl.id = "page-content"
+
+        // add the text node to the newly created section
+        pageContentEl.appendChild(questionOneContent);
+
+        // add the newly created element and its content to the DOM
+        let questionOneSection = document.getElementById("page-content");
+        document.body.insertBefore(pageContentEl, questionOneSection);
+    };
+    questionOne();
+    pageContentEl.remove;
+};
+
+// write question to screen (addContent)
+
+// if else for answer selection
+
+// if true, advance to next question
+// if false, subtract 10 seconds from timer, advance to next question (removeContent, addContent)
 
 // if all questions are answered or timer <= 0, game ends
 
-    // if score < 0, prompt to save initials and score
+// if score < 0, prompt to save initials and score
 
-        // store score and initials in local storage, display high scores
+// store score and initials in local storage, display high scores
 
-    // prompt user to take quiz again
+// prompt user to take quiz again
 
 // function to click start button and initiate quiz and timer
 
 let startButtonEl = document.getElementById("start-btn");
 
-startButtonEl.addEventListener("click",() => {
+startButtonEl.addEventListener("click", () => {
     startQuiz();
     startTimer();
 });
