66e9f6b0bb91d7ce9c4755df2d102e5a53b0b846
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-06-15 08:28:23 -0500

    Add all quiz questions and question functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -151,17 +151,23 @@ let questionThree = function() {
     
     let answerWrong = document.createElement("button");
     answerWrong.className = "btn wrong"
-    answerWrong.textContent = "for (i = 1, i < arrayName.length, i++)"
+    answerWrong.setAttribute("style", "white-space: pre;");
+    answerWrong.textContent = "let buttonEl = document.getElementById('btn-id'); \r\n"
+    answerWrong.textContent += "buttonEl.addEventListener('click', helloAlert);"
     questionThreeContentEl.append(answerWrong);
 
     let answerWrongTwo = document.createElement("button");
     answerWrongTwo.className = "btn wrong"
-    answerWrongTwo.textContent = "for (i = 0, i < arrayName.length, i+)"
+    answerWrongTwo.setAttribute("style", "white-space: pre;");
+    answerWrongTwo.textContent = "let buttonEl = document.getButtonByBtn-Id; \r\n"
+    answerWrongTwo.textContent += "buttonEl.addEventListener('mouseover', helloAlert);"
     questionThreeContentEl.append(answerWrongTwo);
  
     let answerWrongThree = document.createElement("button");
     answerWrongThree.className = "btn wrong"
-    answerWrongThree.textContent = "for (i = 0, i < arrayName, i++)"
+    answerWrongThree.setAttribute("style", "white-space: pre;");
+    answerWrongThree.textContent = "let buttonMouseoverEl = document.getElementById('btn-id'); \r\n"
+    answerWrongThree.textContent += "buttonEl.addEventListener('mouseover', helloAlert);"
     questionThreeContentEl.append(answerWrongThree);
     
     // if correct answer chosen
@@ -185,6 +191,124 @@ let questionThree = function() {
         wrongAnswer();
         questionThreeContentEl.remove();
         questionFour();
+    });
+};
+
+// question four function
+let questionFour = function() {    
+    // create new div element, add id
+    let questionFourContentEl = document.createElement("section");
+    questionFourContentEl.id = "question-four-content"
+    document.body.append(questionFourContentEl); 
+    
+    // adding h3 to new section element for T/F statement
+    let questionFourHeader = document.createElement("h3");
+    questionFourHeader.id = "question-header"
+    questionFourHeader.textContent = "Which of the following will add 'rutabaga' to an array named theseAreVegetables?"
+    questionFourContentEl.append(questionFourHeader);
+    
+    // adding possible answers beneath question
+    let answerWrong = document.createElement("button");
+    answerWrong.className = "btn wrong"
+    answerWrong.textContent = "This is not possible to do outside of the original definition of an array."
+    questionFourContentEl.append(answerWrong);
+
+    let answerWrongTwo = document.createElement("button");
+    answerWrongTwo.className = "btn wrong"
+    answerWrongTwo.textContent = "theseAreVegetables.addString('rutabaga');"
+    questionFourContentEl.append(answerWrongTwo);
+ 
+    let answerWrongThree = document.createElement("button");
+    answerWrongThree.className = "btn wrong"
+    answerWrongThree.textContent = "addToArray.theseAreVegetables('rutabaga');"
+    questionFourContentEl.append(answerWrongThree);
+
+    let answerCorrect = document.createElement("button");
+    answerCorrect.className = "btn"
+    answerCorrect.id = "answer-correct"
+    answerCorrect.textContent = "theseAreVegetables.push('rutabaga');"
+    questionFourContentEl.append(answerCorrect);
+    
+    // if correct answer chosen
+    answerCorrect.addEventListener("click", () => {
+        questionFourContentEl.remove();
+        questionFive();
+    });
+    
+    // if incorrect answers chosen
+    answerWrong.addEventListener("click", () => {
+        wrongAnswer();
+        questionFourContentEl.remove();
+        questionFive();
+    });
+    answerWrongTwo.addEventListener("click", () => {
+        wrongAnswer();
+        questionFourContentEl.remove();
+        questionFive();
+    });
+    answerWrongThree.addEventListener("click", () => {
+        wrongAnswer();
+        questionFourContentEl.remove();
+        questionFive();
+    });
+};
+
+// question five function
+let questionFive = function() {    
+    // create new div element, add id
+    let questionFiveContentEl = document.createElement("section");
+    questionFiveContentEl.id = "question-five-content"
+    document.body.append(questionFiveContentEl); 
+    
+    // adding h3 to new section element for T/F statement
+    let questionFiveHeader = document.createElement("h3");
+    questionFiveHeader.id = "question-header"
+    questionFiveHeader.textContent = "To send an item to local storage, which of the following would be used?"
+    questionFiveContentEl.append(questionFiveHeader);
+    
+    // adding possible answers beneath question
+    let answerWrong = document.createElement("button");
+    answerWrong.className = "btn wrong"
+    answerWrong.textContent = "localStorage.getItem"
+    questionFiveContentEl.append(answerWrong);
+
+    let answerWrongTwo = document.createElement("button");
+    answerWrongTwo.className = "btn wrong"
+    answerWrongTwo.textContent = "saveItem.localStorage"
+    questionFiveContentEl.append(answerWrongTwo);
+
+    let answerCorrect = document.createElement("button");
+    answerCorrect.className = "btn"
+    answerCorrect.id = "answer-correct"
+    answerCorrect.textContent = "localStorage.setItem"
+    questionFiveContentEl.append(answerCorrect);
+ 
+    let answerWrongThree = document.createElement("button");
+    answerWrongThree.className = "btn wrong"
+    answerWrongThree.textContent = "localStorage.saveItem"
+    questionFiveContentEl.append(answerWrongThree);
+    
+    // if correct answer chosen
+    answerCorrect.addEventListener("click", () => {
+        questionFiveContentEl.remove();
+        returnScore();
+    });
+    
+    // if incorrect answers chosen
+    answerWrong.addEventListener("click", () => {
+        wrongAnswer();
+        questionFiveContentEl.remove();
+        returnScore();
+    });
+    answerWrongTwo.addEventListener("click", () => {
+        wrongAnswer();
+        questionFiveContentEl.remove();
+        returnScore();
+    });
+    answerWrongThree.addEventListener("click", () => {
+        wrongAnswer();
+        questionFiveContentEl.remove();
+        returnScore();
     });
 };
 
